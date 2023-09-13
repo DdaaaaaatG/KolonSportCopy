2da8d496bf465f464bd091b1330d76c9b04f2f4e
Woon <85141928+DdaaaaaatG@users.noreply.github.com>
2023-09-13 09:48:16 +0900

    build ver 1.0

--- src/route/Private.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ProducDetailtpage from '../pages/ProducDetailtpage'
-import {Navigate } from 'react-router-dom';
-function Private({authenticate}) {
-  return authenticate == true? <ProducDetailtpage/>:<Navigate to='/pages/Login'/>
-}
-
-export default Private
